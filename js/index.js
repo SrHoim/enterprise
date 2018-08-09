d53076e486b9000a805161f9b5eda0910b375c85
SrHoim <1018823281@qq.com>
2018-08-09 15:04:48 +0800

    jpccc

--- js/index.js
+++ js/index.js
@@ -1,43 +1,45 @@
-window.onload=function(){
-	let bannerBox=document.getElementsByClassName('banner-picture')[0];
-	let imgs=bannerBox.getElementsByTagName('img');
-	let firstGo=document.getElementsByClassName('first');
-	let lastGo=document.getElementsByClassName('last');
+window.onload = function () {
+	//轮播图
+	let bannerBox = document.getElementsByClassName('banner-picture')[0];
+	let imgs = bannerBox.getElementsByTagName('img');
+	let firstGo = document.getElementsByClassName('first');
+	let lastGo = document.getElementsByClassName('last');
 
-	let t=setInterval(move,3000);
-	let num=0;
-	function move(){
+	let t = setInterval(move, 3000);
+	let num = 0;
+
+	function move() {
 		num++;
-		if(num==imgs.length){
-			num=0;
+		if (num == imgs.length) {
+			num = 0;
 		}
-		for(let i=0;i<imgs.length;i++){
-			imgs[i].style.zIndex=5;
+		for (let i = 0; i < imgs.length; i++) {
+			imgs[i].style.zIndex = 5;
 		}
-		imgs[num].style.zIndex=10;
+		imgs[num].style.zIndex = 10;
 	}
 
-	bannerBox.onmouseenter=function(){
+	bannerBox.onmouseenter = function () {
 		clearInterval(t);
 	}
-	bannerBox.onmouseleave=function(){
-		t=setInterval(move,3000);
+	bannerBox.onmouseleave = function () {
+		t = setInterval(move, 3000);
 	}
 
-	function move1(){
+	function move1() {
 		num--;
-		if(num<0){
-			num=imgs.length-1;
+		if (num < 0) {
+			num = imgs.length - 1;
 		}
-		for(let i=0;i<imgs.length;i++){
-			imgs[i].style.zIndex=5;
+		for (let i = 0; i < imgs.length; i++) {
+			imgs[i].style.zIndex = 5;
 		}
-		imgs[num].style.zIndex=10;
+		imgs[num].style.zIndex = 10;
 	}
-	firstGo[0].onclick=function(){
+	firstGo[0].onclick = function () {
 		move1();
 	}
-	lastGo[0].onclick=function(){
+	lastGo[0].onclick = function () {
 		move();
 	}
 }
